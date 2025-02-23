15ce70a1e5e8226dd47d42134a848d9479a7916a
amanvile <deribaman@gmail.com>
2025-02-23 06:56:24 +0300

    changed app bar to green

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=1;
+  int rightDiceNumber=3;
+
+  @override
+
+
   Widget build(BuildContext context) {
-    return Container();
+
+  void diceRoll(){
+    leftDiceNumber=Random().nextInt(5)+1;
+    rightDiceNumber=Random().nextInt(5)+1;
+
+
+  }
+
+    return SafeArea(
+
+      child:
+
+
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Row(
+
+                  // mainAxisAlignment: MainAxisAlignment.center,
+
+
+                    children: [ Expanded(child: TextButton(onPressed:(){setState(() {
+                      diceRoll();
+                    });},child:Image.asset("images/dice$leftDiceNumber.png"))),
+                                Expanded(child:TextButton(onPressed:(){setState(() {
+                                  diceRoll();
+                                });},child: Image.asset("images/dice$rightDiceNumber.png"))),
+                    ],
+                ),
+              ),
+            ),
+
+
+
+    );
   }
 }
